810b017e4a4c1373738ac35bba19bf88c096256f
Jeremy Kenedy <jeremykenedy@gmail.com>
2019-03-12 03:30:06 +0000

    Apply fixes from StyleCI

--- src/database/seeds/publish/BlockedItemsTableSeeder.php
+++ src/database/seeds/publish/BlockedItemsTableSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeds;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use jeremykenedy\LaravelBlocker\App\Models\BlockedItem;
 use jeremykenedy\LaravelBlocker\App\Models\BlockedType;
