a0565dbd3770cc670504ae84f4707d2d4dac590f
Max Destors <max.destors@gmail.com>
2026-08-26 14:16:12 +0200

    l13

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use jeremykenedy\LaravelBlocker\App\Http\Controllers\LaravelBlockerController;
+use jeremykenedy\LaravelBlocker\App\Http\Controllers\LaravelBlockerDeletedController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,20 +13,19 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'middleware'    => ['web', 'checkblocked'],
     'as'            => 'laravelblocker::',
-    'namespace'     => 'jeremykenedy\LaravelBlocker\App\Http\Controllers',
 ], function () {
     // Blocker
-    Route::resource('blocker', 'LaravelBlockerController');
+    Route::resource('blocker', LaravelBlockerController::class);
 
     // Blocker Soft Deleted
-    Route::get('blocker-deleted', 'LaravelBlockerDeletedController@index')->name('blocker-deleted');
-    Route::get('blocker-deleted/{id}', 'LaravelBlockerDeletedController@show')->name('blocker-item-show-deleted');
-    Route::put('blocker-deleted/{id}', 'LaravelBlockerDeletedController@restoreBlockedItem')->name('blocker-item-restore');
-    Route::post('blocker-deleted-restore-all', 'LaravelBlockerDeletedController@restoreAllBlockedItems')->name('blocker-deleted-restore-all');
-    Route::delete('blocker-deleted/{id}', 'LaravelBlockerDeletedController@destroy')->name('blocker-item-destroy');
-    Route::delete('blocker-deleted-destroy-all', 'LaravelBlockerDeletedController@destroyAllItems')->name('destroy-all-blocked');
+    Route::get('blocker-deleted', [LaravelBlockerDeletedController::class, 'index'])->name('blocker-deleted');
+    Route::get('blocker-deleted/{id}', [LaravelBlockerDeletedController::class, 'show'])->name('blocker-item-show-deleted');
+    Route::put('blocker-deleted/{id}', [LaravelBlockerDeletedController::class, 'restoreBlockedItem'])->name('blocker-item-restore');
+    Route::post('blocker-deleted-restore-all', [LaravelBlockerDeletedController::class, 'restoreAllBlockedItems'])->name('blocker-deleted-restore-all');
+    Route::delete('blocker-deleted/{id}', [LaravelBlockerDeletedController::class, 'destroy'])->name('blocker-item-destroy');
+    Route::delete('blocker-deleted-destroy-all', [LaravelBlockerDeletedController::class, 'destroyAllItems'])->name('destroy-all-blocked');
 
     // Blocker Search
-    Route::post('search-blocked', 'LaravelBlockerController@search')->name('search-blocked');
-    Route::post('search-blocked-deleted', 'LaravelBlockerDeletedController@search')->name('search-blocked-deleted');
+    Route::post('search-blocked', [LaravelBlockerController::class, 'search'])->name('search-blocked');
+    Route::post('search-blocked-deleted', [LaravelBlockerDeletedController::class, 'search'])->name('search-blocked-deleted');
 });
