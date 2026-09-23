e92e74835690159670f00719024ae76e0349b862
Jeremy Kenedy <jeremykenedy@gmail.com>
2026-09-22 18:49:47 -0700

    Preserve legacy jQuery compatibility and fix confirmation dialogs

--- src/resources/views/modals/confirm-modal.blade.php
+++ src/resources/views/modals/confirm-modal.blade.php
@@ -11,11 +11,11 @@
         $btnSubmitText = trans('laravelblocker::laravelblocker.modals.btnConfirm');
     }
 @endphp
-<div class="modal fade modal-{{$modalClass}}" id="{{$formTrigger}}" role="dialog" aria-labelledby="{{$formTrigger}}Label" aria-hidden="true">
+<div class="modal fade modal-{{$modalClass}}" id="{{$formTrigger}}" role="dialog" aria-labelledby="{{$formTrigger}}Label">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header {{$modalClass}}">
-                <h5 class="modal-title">
+                <h5 class="modal-title" id="{{$formTrigger}}Label">
                     Confirm
                 </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
